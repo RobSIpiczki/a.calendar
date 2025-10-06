da83ab2c87a4b249cdbbda10b49bc175cec331bf
Rob.Sipiczki <robert.e.sipiczki@gmail.com>
2025-10-06 23:09:05 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+const calendarButton = document.querySelector("#generateCalendarBtn");
+const calendar = document.querySelector(".calendar");
+
+const openDoor = (path, event) => {
+    event.target.parentNode.style.backgroundImage = "url('" + path + "')";
+};
+
+const calendarDays = 20;
+const createCalendar = () => {
+    calendar.innerHTML = ""; // Clear previous doors
+    for (let i = 0; i < calendarDays; i++) {
+        const calendarDoor = document.createElement("div");
+        const calendarDoorText = document.createElement("div");
+        calendarDoor.classList.add("img");
+        calendarDoor.style.gridArea = "door" + (i + 1);
+        calendar.appendChild(calendarDoor);
+        calendarDoorText.classList.add("text");
+        calendarDoorText.innerHTML = (i + 1);
+        calendarDoor.appendChild(calendarDoorText);
+        const imgNumber = i + 1;
+        const imgPath = "images/" + imgNumber + ".jpg";
+        calendarDoorText.addEventListener("click", openDoor.bind(null, imgPath));
+    }
+};
+
+calendarButton.addEventListener("click", createCalendar);
