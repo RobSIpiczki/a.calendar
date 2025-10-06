c7323cc73ed3e6cff88449afb5ccc09337a9c1c3
Rob.Sipiczki <robert.e.sipiczki@gmail.com>
2025-10-06 23:13:35 +0200

    Update main.js

--- main.js
+++ main.js
@@ -18,10 +18,11 @@ const createCalendar = () => {
         calendarDoorText.innerHTML = (i + 1);
         calendarDoor.appendChild(calendarDoorText);
         const imgNumber = i + 1;
-        const imgPath =  imgNumber + ".jpg";
+        const imgPath = `${imgNumber}.jpg`;
         calendarDoorText.addEventListener("click", openDoor.bind(null, imgPath));
     }
 };
 
 
 calendarButton.addEventListener("click", createCalendar);
+
